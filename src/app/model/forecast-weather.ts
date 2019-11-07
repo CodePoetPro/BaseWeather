6058c89c9d48f223eec054d4cf68a3795febb1cb
codepoetpro <codepoetpro@Tr1pps-MacBook-Air-3.local>
2019-11-07 13:33:59 +0100

    Build FIX : Added humidity to ForecastWeather model

--- src/app/model/forecast-weather.ts
+++ src/app/model/forecast-weather.ts
@@ -2,7 +2,7 @@ export interface ForecastWeather {
 clouds?: any;
 dt: number;
 dt_txt: string;
-main: {temp: number, temp_min: number, temp_max: number, pressure: number, sea_level?: number, grnd_level?: number, temp_kf: number};
+main: {temp: number, temp_min: number, temp_max: number, humidity: number, pressure: number, sea_level?: number, grnd_level?: number, temp_kf: number};
 sys?: any;
 weather: {id: number, main: string, description: string, icon: string}[]
 wind: {speed: number, deg: number}

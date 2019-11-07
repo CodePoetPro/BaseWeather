ba65a15feb984305460b15a1c273516f411c4d86
codepoetpro <codepoetpro@Tr1pps-MacBook-Air-3.local>
2019-11-07 19:55:07 +0100

    Update prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseURL : "http://api.openweathermap.org/data/2.5",
+  baseURL : "https://api.openweathermap.org/data/2.5",
   OWKEY : "6e0defaa612d85637e3169c8998048bb"
 };
